5d3069b143ef79bee246533bf51f752388a79504
Jonathan May <jmay4826@gmail.com>
2018-08-27 10:15:42 -0500

    move routes to app.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,18 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Setup from "./components/setup";
-import Classroom from "./components/classroom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/:id" component={Classroom} />
-      <Route path="/" component={Setup} />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
