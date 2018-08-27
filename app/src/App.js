496da03764f1aa61f9680b771e9ab3c35786f1c1
Jonathan May <jmay4826@gmail.com>
2018-08-27 10:15:54 -0500

    move routes to app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,56 +1,18 @@
 import React, { Component } from "react";
-import "./App.css";
-import openSocket from "socket.io-client";
-const socket = openSocket();
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Setup from "./components/setup";
+import Classroom from "./components/Classroom";
+import EnterClassroom from "./components/EnterClassroom";
+import Instructor from "./components/Instructor";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [],
-      questions: []
-    };
-    socket.on("user list", users => this.setState({ users }));
-    socket.on("question added", questions => this.setState({ questions }));
-    this.editUsername = this.editUsername.bind(this);
-    this.askQuestion = this.askQuestion.bind(this);
-    this.makeAdmin = this.makeAdmin.bind(this);
-  }
-
-  editUsername() {
-    socket.emit("editUsername", this.state.username);
-  }
-  askQuestion() {
-    socket.emit("question", this.state.question);
-  }
-  makeAdmin() {
-    socket.emit("makeAdmin");
-  }
-
-  render() {
-    return (
-      <div>
-        <p>
-          <input
-            style={{ border: "1px solid black" }}
-            type="text"
-            onChange={e => {
-              this.setState({ username: e.target.value });
-            }}
-          />
-          <button onClick={this.editUsername}>Edit Username</button>
-          <br />
-          <button onClick={this.makeAdmin}>Make Admin</button>
-        </p>
-        <ul>{this.state.users.map(user => <li>{user.name}</li>)}</ul>
-        <textarea onChange={e => this.setState({ question: e.target.value })}>
-          {this.state.question}
-        </textarea>
-        <button onClick={this.askQuestion}>Ask Question</button>
-        <div>{JSON.stringify(this.state.questions)}</div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/:id/instructor" component={Instructor} />
+      <Route path="/:id" component={Classroom} />
+      <Route path="/" component={EnterClassroom} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
