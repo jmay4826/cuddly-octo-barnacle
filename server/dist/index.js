fd28eb0c66e732793649ad7df0c8da50f81be5fd
Jonathan May <jmay4826@gmail.com>
2018-09-04 09:59:29 -0600

    Convert to handy-redis for better typescript support

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,26 +3,54 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
+const handy_redis_1 = require("handy-redis");
 const http_1 = require("http");
-const redis = require("redis");
 const socketIO = require("socket.io");
-const util_1 = require("util");
 const app = express();
 const http = new http_1.Server(app);
 const io = socketIO(http);
-const client = redis.createClient();
-const flushallAsync = util_1.promisify(client.flushall).bind(client);
-const hgetallAsync = util_1.promisify(client.hgetall).bind(client);
-const hmsetAsync = util_1.promisify(client.hmset).bind(client);
-const hgetAsync = util_1.promisify(client.hget).bind(client);
-const sremAsync = util_1.promisify(client.srem).bind(client);
-const saddAsync = util_1.promisify(client.sadd).bind(client);
-const smembersAsync = util_1.promisify(client.smembers).bind(client);
-flushallAsync();
+const client = handy_redis_1.createHandyClient();
+// const flushallAsync = promisify(client.flushall).bind(client) as () => Promise<
+//   string
+// >;
+// const hgetallAsync = promisify(client.hgetall).bind(client) as (
+//   key: string
+// ) => Promise<{ [key: string]: string }>;
+// const hmsetAsync = promisify(client.hmset).bind(client) as (
+//   hash: string | number,
+//   values: object
+// ) => Promise<boolean>;
+// const hgetAsync = promisify(client.hget).bind(client) as (
+//   key: string,
+//   field: string
+// ) => Promise<string>;
+// const sremAsync = promisify(client.srem).bind(client) as (
+//   hash: string,
+//   ...values: string[]
+// ) => Promise<number>;
+// const saddAsync = promisify(client.sadd).bind(client) as (
+//   key: string | number,
+//   ...members: string[]
+// ) => Promise<number>;
+// const smembersAsync = promisify(client.smembers).bind(client) as (
+//   key: string | number
+// ) => Promise<string[]>;
+// flushallAsync();
+client.flushall();
 const filterUsers = (users) => {
     const students = users.filter(({ instructor }) => !instructor);
     const instructors = users.filter(({ instructor }) => instructor);
     return { students, instructors };
+};
+const objToArray = (input) => {
+    const obj = input;
+    const result = [];
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            result.push([key, obj[key].toString()]);
+        }
+    }
+    return result;
 };
 const convertBooleans = async (input) => {
     const obj = await input;
@@ -38,28 +66,33 @@ const convertBooleans = async (input) => {
     }
     return obj;
 };
-const getAllUsersByClassroom = (classroom) => smembersAsync(classroom).then(async (users) => Promise.all(users.map(async (user) => hgetallAsync(user)).map(convertBooleans)));
+const getAllUsersByClassroom = (classroom) => client
+    .smembers(classroom)
+    .then(async (users) => Promise.all(users.map(async (user) => client.hgetall(user)).map(convertBooleans)));
 io.on("connection", socket => {
     socket.on("join classroom", (user) => {
         socket.join(user.classroom.toString());
         if (user.instructor) {
             socket.join(user.classroom + "-instructors");
         }
-        hmsetAsync(socket.id, user).catch(console.log);
-        saddAsync(user.classroom, socket.id)
+        client.hmset(socket.id, ...objToArray(user)).catch(console.log);
+        client
+            .sadd(user.classroom, socket.id)
             .then(_ => getAllUsersByClassroom(user.classroom))
             .then(users => io.to(user.classroom.toString()).emit("new user", filterUsers(users)));
     });
     socket.on("disconnect", async (reason) => {
-        const classroom = await hgetAsync(socket.id, "classroom");
-        client.del(socket.id, console.log);
-        sremAsync(classroom, socket.id)
+        const classroom = (await client.hget(socket.id, "classroom")) || "";
+        client.del(socket.id);
+        client
+            .srem(classroom, socket.id)
             .then(result => getAllUsersByClassroom(classroom))
             .then(users => io.to(classroom).emit("new user", filterUsers(users)))
             .catch(console.log);
     });
     socket.on("update user", user => {
-        hmsetAsync(user.id, user)
+        client
+            .hmset(user.id, user)
             .then(_ => getAllUsersByClassroom(user.classroom))
             .then(users => {
             io.to(user.classroom).emit("new user", filterUsers(users));
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,14 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import * as express from "express";
+import { createHandyClient } from "handy-redis";
 import { Server } from "http";
-import * as redis from "redis";
 import * as socketIO from "socket.io";
-import { promisify } from "util";
+
 const app = express();
 const http = new Server(app);
 const io = socketIO(http);
-const client = redis.createClient();
+const client = createHandyClient();
 
 interface IUser {
   status: string;
@@ -19,38 +19,50 @@ interface IUser {
   raisedHand: boolean;
 }
 
-const flushallAsync = promisify(client.flushall).bind(client) as () => Promise<
-  string
->;
-const hgetallAsync = promisify(client.hgetall).bind(client) as (
-  key: string
-) => Promise<{ [key: string]: string }>;
-const hmsetAsync = promisify(client.hmset).bind(client) as (
-  hash: string | number,
-  values: object
-) => Promise<boolean>;
-const hgetAsync = promisify(client.hget).bind(client) as (
-  key: string,
-  field: string
-) => Promise<string>;
-const sremAsync = promisify(client.srem).bind(client) as (
-  hash: string,
-  ...values: string[]
-) => Promise<number>;
-const saddAsync = promisify(client.sadd).bind(client) as (
-  key: string | number,
-  ...members: string[]
-) => Promise<number>;
-const smembersAsync = promisify(client.smembers).bind(client) as (
-  key: string | number
-) => Promise<string[]>;
+// const flushallAsync = promisify(client.flushall).bind(client) as () => Promise<
+//   string
+// >;
+// const hgetallAsync = promisify(client.hgetall).bind(client) as (
+//   key: string
+// ) => Promise<{ [key: string]: string }>;
+// const hmsetAsync = promisify(client.hmset).bind(client) as (
+//   hash: string | number,
+//   values: object
+// ) => Promise<boolean>;
+// const hgetAsync = promisify(client.hget).bind(client) as (
+//   key: string,
+//   field: string
+// ) => Promise<string>;
+// const sremAsync = promisify(client.srem).bind(client) as (
+//   hash: string,
+//   ...values: string[]
+// ) => Promise<number>;
+// const saddAsync = promisify(client.sadd).bind(client) as (
+//   key: string | number,
+//   ...members: string[]
+// ) => Promise<number>;
+// const smembersAsync = promisify(client.smembers).bind(client) as (
+//   key: string | number
+// ) => Promise<string[]>;
 
-flushallAsync();
+// flushallAsync();
+client.flushall();
 
 const filterUsers = (users: IUser[]) => {
   const students = users.filter(({ instructor }) => !instructor);
   const instructors = users.filter(({ instructor }) => instructor);
   return { students, instructors };
+};
+
+const objToArray = (input: any) => {
+  const obj = input;
+  const result: Array<[string, string]> = [];
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result.push([key, obj[key].toString()]);
+    }
+  }
+  return result;
 };
 
 const convertBooleans = async (input: Promise<{}>) => {
@@ -65,16 +77,17 @@ const convertBooleans = async (input: Promise<{}>) => {
       }
     }
   }
-  return obj;
+  return obj as IUser;
 };
 
 const getAllUsersByClassroom = (classroom: string) =>
-  smembersAsync(classroom).then(
-    async (users: string[]) =>
+  client
+    .smembers(classroom)
+    .then(async (users: string[]) =>
       Promise.all(
-        users.map(async user => hgetallAsync(user)).map(convertBooleans)
-      ) as Promise<IUser[]>
-  );
+        users.map(async user => client.hgetall(user)).map(convertBooleans)
+      )
+    );
 
 io.on("connection", socket => {
   socket.on("join classroom", (user: IUser) => {
@@ -82,8 +95,9 @@ io.on("connection", socket => {
     if (user.instructor) {
       socket.join(user.classroom + "-instructors");
     }
-    hmsetAsync(socket.id, user).catch(console.log);
-    saddAsync(user.classroom, socket.id)
+    client.hmset(socket.id, ...objToArray(user)).catch(console.log);
+    client
+      .sadd(user.classroom, socket.id)
       .then(_ => getAllUsersByClassroom(user.classroom))
       .then(users =>
         io.to(user.classroom.toString()).emit("new user", filterUsers(users))
@@ -91,17 +105,19 @@ io.on("connection", socket => {
   });
 
   socket.on("disconnect", async reason => {
-    const classroom = await hgetAsync(socket.id, "classroom");
+    const classroom = (await client.hget(socket.id, "classroom")) || "";
 
-    client.del(socket.id, console.log);
-    sremAsync(classroom, socket.id)
+    client.del(socket.id);
+    client
+      .srem(classroom, socket.id)
       .then(result => getAllUsersByClassroom(classroom))
       .then(users => io.to(classroom).emit("new user", filterUsers(users)))
       .catch(console.log);
   });
 
   socket.on("update user", user => {
-    hmsetAsync(user.id, user)
+    client
+      .hmset(user.id, user)
       .then(_ => getAllUsersByClassroom(user.classroom))
       .then(users => {
         io.to(user.classroom).emit("new user", filterUsers(users));
